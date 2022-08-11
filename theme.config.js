754312bbae2b372bce34d87875e9de09e0efd456
Justin Schreiber <jschreiber234@gmail.com>
2022-08-11 15:15:38 -0400

    changes to footer

--- theme.config.js
+++ theme.config.js
@@ -4,14 +4,17 @@ export default {
   footer: (
     <footer>
       <small>
-        <time>{YEAR}</time> © Justin Schreiber.
-        <a href="/feed.xml">RSS</a>
+        <time>{YEAR}</time> © Justin Schreiber. Powered by <nbps></nbps>
+        <a href="https://nextjs.org/">Next.js</a> and <nbps></nbps>
+        <a href="https://nextra.vercel.app/">nextra</a>
+        <right><a href="/feed.xml">RSS</a></right>
+        
       </small>
       <style jsx>{`
         footer {
           margin-top: 8rem;
         }
-        a {
+        right {
           float: right;
         }
       `}</style>
