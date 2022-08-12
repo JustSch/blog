874907228df5fd50299e0b93bf63bf3a76967336
Justin Schreiber <jschreiber234@gmail.com>
2022-08-12 19:49:15 -0400

    fix text alignment

--- theme.config.js
+++ theme.config.js
@@ -3,12 +3,13 @@ const YEAR = new Date().getFullYear()
 export default {
   footer: (
     <footer>
-      <small>
-        <time>{YEAR}</time> © Justin Schreiber. Powered by {' '}
-        <a href="https://nextjs.org/">Next.js</a> and {' '}
-        <a href="https://nextra.vercel.app/">nextra</a>
-        <a className="right" href="/blog/feed.xml">RSS</a>
-        
+      <small >
+        <div className="left">
+          <time >{YEAR}</time> © Justin Schreiber. Powered by {' '}
+          <a href="https://nextjs.org/">Next.js</a> and {' '}
+          <a href="https://nextra.vercel.app/">nextra</a>         
+        </div>
+        <a className="right" href="/blog/feed.xml">RSS</a>        
       </small>
       <style jsx>{`
         footer {
